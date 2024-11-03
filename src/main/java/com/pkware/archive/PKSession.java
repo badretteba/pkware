da17cf50f906bee70ce250827f1b53d8e74e7632
badretteba <badrettebaa@gmail.com>
2024-11-03 22:03:39 +0100

    first commit

---
+++ src/main/java/com/pkware/archive/PKSession.java
@@ -0,0 +1,19 @@
+package com.pkware.archive;
+
+public class PKSession {
+    private String license;
+
+    public PKSession(String license) {
+        this.license = license;
+    }
+
+    // You can add more methods as needed for stubbing purposes
+    public String getLicense() {
+        return license;
+    }
+
+    // Add other methods that might be called in your code
+    public void someOperation() {
+        // Stub method for some operation
+    }
+}
